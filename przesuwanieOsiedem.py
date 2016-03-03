08182ec829e592b7b7ad3075ddeb67a93297be6b
jahooo <okraskaj@ee.pw.edu.pl>
2016-03-03 17:41:27 +0100

    Odszyfrowywanie przesuniec o 7 w miare dziala. Wypisuje wszystkie mozliwosci

--- przesuwanieOsiedem.py
+++ przesuwanieOsiedem.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
+import string
 
-tekst_jawny = "To jest tekst jawny i jest on bardzo jawny i czytelny. A dodatkowo sensowny"
-przesuniecie = 7
+tekst_jawny = "tekst jawny niezaszyfrowany super fajny i malo tajny"
 
-def zaszyfruj(jawny,klucz):
-    zaszyfrowany = ""
-    for i in range(len(tekst_jawny)):
-        if ord(jawny[i]) > 122 - klucz:
-            zaszyfrowany += chr(ord(jawny[i]) + klucz - 26)
-        else:
-            zaszyfrowany += chr(ord(jawny[i]) + klucz)
-    return zaszyfrowany
+table = string.maketrans('abcdefghijklmnopqrstuwxyz','hijklmnopqrstuwxyzabcdefg')
+reverse_table = string.maketrans('abcdefghijklmnopqrstuwxyz','stuwxyzabcdefghijklmnopqr')
 
 
-def odszyfruj(zaszyfrowany, przesuniecie):
-    for i in range(len(zaszyfrowany)):
-        if ord(zaszyfrowany[i]) > 122 - klucz:
-            zaszyfrowany += chr(ord(jawny[i]) + klucz - 26)
-        else:
-            zaszyfrowany += chr(ord(jawny[i]) + klucz)
-    return zaszyfrowany
+def przesun(tekst, tabela):
+    przesuniety = ''
+    for line in tekst:
+        przesuniety += string.translate(line,tabela)
+    return przesuniety
 
-print odszyfruj(zaszyfruj(tekst_jawny,7),7)
+def sprawdzPrzesuniecia(zaszyfrowany):
+    for i in range(1,5):
+        temp = zaszyfrowany
+        for j in range(1,i):
+            temp = przesun(temp,table)
+        print temp
 
+zaszyfrowany = przesun(tekst_jawny,reverse_table)
+zaszyfrowany = przesun(zaszyfrowany, reverse_table)
+sprawdzPrzesuniecia(zaszyfrowany)
