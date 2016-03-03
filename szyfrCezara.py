3953c414e82455bf5896fb95252b7ed11957fd4d
jahooo <okraskaj@ee.pw.edu.pl>
2016-03-03 17:22:19 +0100

    Poprawiony szyfr cezara teraz dziala tak, jak wymaga od nas ćwiczenie

--- szyfrCezara.py
+++ szyfrCezara.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
+import fileinput
+import string
 
-tekst_jawny = "To jest tekst jawny i jest on bardzo jawny i czytelny. A dodatkowo sensowny"
-klucz = 31
+table = string.maketrans('abcdefghijklmnopqrstuwxyz ','ghijklmnopqrstuwxyzabcdef ')
+tekst_jawny = "tekst jawny niezaszyfrowany super fajny i malo tajny"
 
-
-i=0
-
-def zaszyfruj(jawny,klucz):
-    zaszyfrowany = ""
-    for i in range(len(tekst_jawny)):
-        if ord(jawny[i]) > 122 - klucz:
-            zaszyfrowany += chr(ord(jawny[i]) + klucz - 26)
-        else:
-            zaszyfrowany += chr(ord(jawny[i]) + klucz)
+def zaszyfruj(jawny):
+    zaszyfrowany = ''
+    for line in jawny:
+        #line = line.rstrip()
+        zaszyfrowany += string.translate(line,table)
     return zaszyfrowany
 
-    i+=1
-
-print zaszyfruj(tekst_jawny,klucz)
+print zaszyfruj(tekst_jawny)
 
